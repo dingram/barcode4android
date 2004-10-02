debee5fcf62f0c8e88455786c3111f2fb356d36f
jmaerki <jmaerki>
2004-10-02 14:51:43 +0000

    misspelled package renamed.

--- src/java/org/krysalis/barcode4j/impl/int2of5/Interleaved2Of5.java
+++ src/java/org/krysalis/barcode4j/impl/int2of5/Interleaved2Of5.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.krysalis.barcode4j.impl.int2fo5;
+package org.krysalis.barcode4j.impl.int2of5;
 
 import org.apache.avalon.framework.configuration.Configurable;
 import org.apache.avalon.framework.configuration.Configuration;
