6550a7a1ce1d8e171eee6ecf5d17fb8642f78ac1
jmaerki <jmaerki>
2007-01-04 08:05:05 +0000

    Code formatting and removal of tab characters.

--- src/fop-0.20.5/java/org/krysalis/barcode4j/fop0205/BarcodeElementMapping.java
+++ src/fop-0.20.5/java/org/krysalis/barcode4j/fop0205/BarcodeElementMapping.java
@@ -34,7 +34,7 @@ public class BarcodeElementMapping implements ElementMapping {
 
     private static final String[] BARCODE_ELEMENTS =
         {"intl2of5", "code39", "codabar", "code128", 
-	     "ean128", "template", "group-separator", "check-digit-marker", "omit-brackets",
+         "ean128", "template", "group-separator", "check-digit-marker", "omit-brackets",
          "upc-a", "upc-e", "ean-13", "ean-8",
          "postnet",
          "height", "module-width", "wide-factor", "quiet-zone",
@@ -74,7 +74,7 @@ public class BarcodeElementMapping implements ElementMapping {
         
         //for compatibility (Krysalis Barcode)
         builder.addMapping(BarcodeConstants.OLD_NAMESPACE, foObjs);
-		builder.addPropertyListBuilder(BarcodeConstants.OLD_NAMESPACE, new DirectPropertyListBuilder());
+        builder.addPropertyListBuilder(BarcodeConstants.OLD_NAMESPACE, new DirectPropertyListBuilder());
     }
 }
 
