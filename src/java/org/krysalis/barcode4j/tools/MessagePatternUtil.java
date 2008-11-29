e75e22219fef67e985d18c0b11d2e2f59450775f
jmaerki <jmaerki>
2008-11-29 16:26:52 +0000

    Patch/Issue #2353976: Support added for a deletion placeholder when formatting the human-readable part. Submitted by: Kalle Sandberg <ks.at.viaduct.se>

--- src/java/org/krysalis/barcode4j/tools/MessagePatternUtil.java
+++ src/java/org/krysalis/barcode4j/tools/MessagePatternUtil.java
@@ -33,7 +33,7 @@ public class MessagePatternUtil {
      * <p>
      * Example: "\_patterned\_:__/__/____" (Any '_' is placeholder for the next message symbol,
      * all other pattern symbols will be inserted between. The '\' is escape char. If the patterned
-     * message is too long you can increase the quite zone lenght to make it visible)
+     * message is too long you can increase the quite zone length to make it visible)
      * @param msg the original message
      * @param pattern the message pattern to be applied
      * @return the formatted message
@@ -44,7 +44,7 @@ public class MessagePatternUtil {
         StringBuffer result = new StringBuffer();
 
         // if there is no pattern then return the original message
-        if ((pattern == null) || "".equals(pattern) 
+        if ((pattern == null) || "".equals(pattern)
                 || msg == null || "".equals(msg)) {
             return msg;
         }
@@ -78,10 +78,12 @@ public class MessagePatternUtil {
             // else
             // append the currentPatternChar to the result and set the
             // escapeCharEncountered flag down
-            if ((!msgFinished) 
-                    && (!escapeCharEncountered) 
-                    && isPlaceholder(currentPatternChar)) {
-                result.append((char) msgBytes[msgIndex]);
+            if ((!msgFinished)
+                    && (!escapeCharEncountered)
+                    && (isPlaceholder(currentPatternChar) || isDeleteholder(currentPatternChar))) {
+                if (!isDeleteholder(currentPatternChar)) {
+                    result.append((char) msgBytes[msgIndex]);
+                }
                 msgIndex++;
                 if (msgIndex == msgBytes.length) {
                     msgFinished = true;
@@ -129,6 +131,18 @@ public class MessagePatternUtil {
 
         result = (c == escapeChar);
 
+        return result;
+    }
+
+    /**
+     * Returns true if the input character is deleteholder.
+     * @param c the input character
+     * @return true if the input character is a position to be deleted
+     */
+    private static boolean isDeleteholder(char c) {
+        boolean result = false;
+        char placeholderChar = '#';
+        result = (placeholderChar == c);
         return result;
     }
 
